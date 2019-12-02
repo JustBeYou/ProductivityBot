93deee556ae269dbfcca252d704835623381f232
JustBeYou <mihailferaru2000@gmail.com>
2019-12-02 20:04:38 +0200

    Setup local environment variables

--- BotAPI/local.sh
+++ BotAPI/local.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [[ -f "functions/.runtimeconfig.json" ]]; then
+    echo "Runtime already configured."
+else
+    firebase functions:config:get > functions/.runtimeconfig.json
+fi
 firebase serve --only hosting,functions
