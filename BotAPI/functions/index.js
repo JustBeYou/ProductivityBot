3a4e79659fdc80ef807106fb625d1e9909a401f5
JustBeYou <mihailferaru2000@gmail.com>
2019-12-03 12:20:31 +0200

    Separated routes for user and bot hooks.

--- BotAPI/functions/index.js
+++ BotAPI/functions/index.js
@@ -11,22 +11,33 @@ var bot = new MessengerBot({
   verify: functions.config().facebook.verify_token,
   app_secret: functions.config().facebook.app_secret
 });
-
 app.use(bodyParser.json());
 
-app.get('/webhook', (req, res) => {
-    console.log("GET", req.body);
-    res.send('{"message" : "ok"}');
+/* Messenger bot webhook */
+app.get('/bot/webhook', (req, res) => {
+    console.log('GET /bot/webhook', req.body);
+    return bot._verify(req, res);
 });
 
-app.post('/webhook', (req, res) => {
-    console.log("POST", req.body, req.body['entry'][0]['changed_fields'], req.body['entry'][0]['messaging']);
-    res.send('{"message" : "ok"}');
+app.post('/bot/webhook', (req, res) => {
+    console.log('POST', req.body, req.body['entry'][0]['changed_fields'], req.body['entry'][0]['messaging']);
+    res.send({status: 'ok'});
 });
 
+/* User webhook */
+app.get('/user/webhook', (req, res) => {
+    console.log('GET /user/webhook', req.body);
+    return bot._verify(req, res); // use the same verification as for the messenger bot
+});
+
+app.get('/user/webhook', (req, res) => {
+    console.log('POST /user/webhook', req.body);
+    res.send({status: 'ok'});
+});
+
+/* Route to check if the service is running */
 app.get('/status', (req, res) => {
-    console.log("Running fine.");
-    res.send("Running fine.");
+    res.send('Running fine.');
 });
 
 exports.app = functions.https.onRequest(app);
