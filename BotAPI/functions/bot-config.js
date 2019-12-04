e537455dce7871f1427d6b0bf297a9e7b1fc5c35
JustBeYou <mihailferaru2000@gmail.com>
2019-12-04 02:27:01 +0200

    Add todo

--- BotAPI/functions/bot-config.js
+++ BotAPI/functions/bot-config.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const messengerBot = require('messenger-bot');
 
+// TODO: renew token, it expires everyday in development mode I think
 const bot = new messengerBot({
   token: functions.config().facebook.page_access_token,
   verify: functions.config().facebook.verify_token,
